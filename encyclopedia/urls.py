5d14a1321170bb5e4d34a2e50d43efbfccc739c3
GabrielKayque <mr.biiiel@hotmail.com>
2022-04-07 21:47:09 -0300

    completed the ENTRY PAGE topic

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki", views.index, name="index"),
+    path("wiki/PageNotFound", views.notfound, name="notfound"),
+    path("wiki/<str:title>", views.title, name="title"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,25 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
-
+import markdown
 from . import util
+from django.urls import reverse
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+    
+def title(request, title=None):
+    if util.get_entry(title) != None:
+        content = markdown.markdown(util.get_entry(title))
+        return render(request, "encyclopedia/title.html", {
+            "title" : title, "content": content 
+        })
+    return HttpResponseRedirect(reverse("notfound"))
+
+
+def notfound(request):
+    return render(request, "encyclopedia/notfound.html")
+    
 
